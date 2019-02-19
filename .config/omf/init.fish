4025565197ce5ea98f34cc4ddf2edafe25c1c462
Matteus Hemström <matteus.hemstrom@valtech.se>
2019-02-19 12:37:26 +0100

    Update init.fish for secrets and yarn

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,3 +1,7 @@
+if test -e ~/.secrets.sh
+  source ~/.secrets.sh
+end
+
 set fish_prompt_pwd_dir_length 2
 
 set -xg EDITOR 'nvim'
@@ -9,6 +13,9 @@ set -xg NVM_DIR '/usr/share/nvm'
 # Set neovim-remote address
 # - (https://github.com/mhinz/neovim-remote)
 # set -xg NVIM_LISTEN_ADDRESS '/tmp/nvimsocket'
+
+# Setup yarn paths
+set -gx PATH $PATH (yarn bin)
 
 alias vim='nvim'
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'
