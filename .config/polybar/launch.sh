0ef78b413f867c3b0b2b33aa68d259ac1d8feb33
Matteus <matteus92@gmail.com>
2020-01-24 21:13:57 +0100

    polybar: host specific config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,5 +7,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Find config file
+CONFIG=$DIR/config-$(hostname)
+if [[ ! -f $CONFIG ]]; then
+	CONFIG=$DIR/config-default
+fi
+
 # Launch bars
-polybar top &
+polybar top --config=$CONFIG &
