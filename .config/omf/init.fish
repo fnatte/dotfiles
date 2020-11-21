3a319061aefa2c54d738cef3ebcea2e1d38014a6
Matteus Hemström <matteus92@gmail.com>
2020-11-21 19:38:10 +0100

    fish: add composer bin path

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -19,6 +19,7 @@ set -xg NVM_DIR '/usr/share/nvm'
 set -gx PATH $PATH $HOME/bin
 set -gx PATH $PATH (yarn bin)
 set -gx PATH $PATH $GOPATH/bin
+set -gx PATH $PATH (composer global config bin-dir --absolute)
 
 # Flutter
 set -gx FLUTTER_HOME /opt/flutter
