fc1b34b51ce03ad29e03ec609a482de1fa9070e1
Matteus Hemström <matteus92@gmail.com>
2021-11-21 21:30:47 +0100

    update bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # Setup oh-my-fish (omf)
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+test ! -f ~/.config/fish/conf.d/omf.fish && curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# Set default shell
+if ! grep -q fish /etc/shells; then
+	echo `which fish` | sudo tee -a /etc/shells
+fi
+chsh -s `which fish`
