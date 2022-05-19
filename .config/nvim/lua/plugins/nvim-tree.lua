e8cb97fc172b622777618ecdbdcaf67b621b2f86
Matteus Hemström <matteus92@gmail.com>
2022-05-19 19:29:20 +0200

    nvim: add some nvim-tree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,13 @@
-require'nvim-tree'.setup {}
+require'nvim-tree'.setup {
+  view = {
+    width = '100%',
+  },
+  actions = {
+    open_file = {
+      quit_on_open = 1,
+    }
+  }
+}
 
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })
 
