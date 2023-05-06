f1e752ff633f96531538cc91d0021d1b4802b61b
Matteus Hemström <matteus92@gmail.com>
2023-05-06 11:16:39 +0200

    nvim: add some keybinding utils

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -12,3 +12,12 @@ vim.cmd([[
 -- Make double-<Esc> clear search highlights and close preview window
 vim.api.nvim_set_keymap('n', '<Esc><Esc>', '<Esc>:nohlsearch<CR><Esc>:pc<CR>:ccl<CR>',
   { noremap = true, silent = true })
+
+-- Move selected lines up/down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor centered when screen scrolling up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
