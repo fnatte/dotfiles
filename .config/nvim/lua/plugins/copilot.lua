9ae20069f0461e8ee3d5243a978c6ccfc45df789
Matteus Hemström <matteus92@gmail.com>
2023-05-07 10:41:09 +0200

    nvim: add plugins/copilot.lua

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+vim.g.copilot_filetypes = {
+  TelescopeResults = false,
+  TelescopePrompt = false,
+  NvimTree = false,
+  NvimTreeNormal = false,
+  NvimTreeEndOfBuffer = false,
+  NvimTreeVertSplit = false,
+  NvimTreeStatusLine = false,
+  NvimTreeStatusLineNC = false,
+  NvimTreeVertSplit = false,
+}
