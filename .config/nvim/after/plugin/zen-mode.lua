458de31c9e91f1bf6a555440b6c2b02779847b63
Matteus Hemström <matteus92@gmail.com>
2023-10-26 19:23:06 +0200

    nvim: add zen-mode

---
+++ .config/nvim/after/plugin/zen-mode.lua
@@ -0,0 +1,35 @@
+local zen_mode = require('zen-mode')
+
+zen_mode.setup({
+  window = {
+    options = {
+      signcolumn = "no",
+      number = false,
+      relativenumber = false,
+      cursorline = false, -- disable cursorline
+      cursorcolumn = false, -- disable cursor column
+      -- foldcolumn = "0", -- disable fold column
+      -- list = false, -- disable whitespace characters
+    },
+  },
+
+  plugins = {
+    gitsigns = { enabled = true },
+    tmux = { enabled = true },
+    kitty = {
+      enabled = true,
+      font = "+4", -- font size increment
+    },
+  },
+
+  on_open = function()
+    vim.cmd("IndentBlanklineDisable")
+  end,
+
+  on_close = function()
+    vim.cmd("IndentBlanklineEnable")
+  end,
+})
+
+vim.keymap.set('n', '<leader>z', zen_mode.toggle, { silent = true })
+
