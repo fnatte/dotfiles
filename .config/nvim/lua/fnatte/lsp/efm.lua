251d688f643ba6758ccd96e99b42f6e3df686194
Matteus Hemström <matteus92@gmail.com>
2024-01-22 20:08:12 +0100

    nvim: efm: use prettier_d instead of format_eslint_d

--- .config/nvim/lua/fnatte/lsp/efm.lua
+++ .config/nvim/lua/fnatte/lsp/efm.lua
@@ -5,12 +5,8 @@ end
 
 local M = {}
 
--- It's sad that we can't just store "formatters" and "linters" under variables
--- from require("efmls-configs.formatters.*") and
--- require("efmls-configs.linters.*")
-
 local prettier = require("efmls-configs.formatters.prettier")
-local format_eslint_d = require("efmls-configs.formatters.eslint_d")
+local prettier_d = require("efmls-configs.formatters.prettier_d")
 local lint_eslint_d = require("efmls-configs.linters.eslint_d")
 
 local languages = {
@@ -24,23 +20,22 @@ local languages = {
     require("efmls-configs.formatters.black"),
   },
 
-  html = { format_eslint_d },
+  html = { prettier_d },
 
   javascript = {
-    format_eslint_d,
+    prettier_d,
     lint_eslint_d,
   },
   javascriptreact = {
-    format_eslint_d,
+    prettier_d,
     lint_eslint_d,
   },
   typescript = {
-    require("efmls-configs.formatters.prettier_d"),
-    format_eslint_d,
+    prettier_d,
     lint_eslint_d,
   },
   typescriptreact = {
-    format_eslint_d,
+    prettier_d,
     lint_eslint_d,
   },
   json = {
