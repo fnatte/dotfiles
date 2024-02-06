b65b247757fde612090844ba2f0a30626bf2a896
Matteus Hemström <matteus.hemstrom@eqtpartners.com>
2024-02-06 10:13:32 +0100

    nvim: plugins: set event to ColorScheme for lualine

--- .config/nvim/lua/fnatte/plugins.lua
+++ .config/nvim/lua/fnatte/plugins.lua
@@ -29,6 +29,7 @@ return {
   },
   {
     'nvim-lualine/lualine.nvim',
+    event = 'ColorScheme',
     config = function()
       require('lualine').setup({ options = { theme = 'rose-pine' } })
     end
