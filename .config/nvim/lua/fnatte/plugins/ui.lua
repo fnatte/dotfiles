86872f92224e4a5e2554c3f278c6d9b31f7ba2f0
Matteus Hemström <matteus92@gmail.com>
2025-10-19 08:53:41 +0200

    nvim: rose-pine config changes

--- .config/nvim/lua/fnatte/plugins/ui.lua
+++ .config/nvim/lua/fnatte/plugins/ui.lua
@@ -2,12 +2,14 @@ return {
   {
     'rose-pine/neovim',
     name = 'rose-pine',
+    lazy = false,
+    priority = 1000,
     config = function()
+      require('rose-pine').setup({
+        dark_variant = 'moon',
+      })
       vim.cmd('colorscheme rose-pine')
     end,
-    opts = {
-      dark_variant = 'moon',
-    },
   },
   {
     'nvim-lualine/lualine.nvim',
